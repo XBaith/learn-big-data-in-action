77796b4c38d9887365fb51e3eaf05e6605a2991f
XBaith <tocreationbai@gmail.com>
2019-08-24 23:21:46 +0800

    Add UnionFindV2 some details

--- java-data-struct/src/tree/union/UnionFindV1.java
+++ java-data-struct/src/tree/union/UnionFindV1.java
@@ -3,8 +3,11 @@ package tree.union;
 public class UnionFindV1 implements UnionFind {
     private int[] unionId;
 
-    public UnionFindV1(int size){
+    public UnionFindV1(int size) {
         unionId = new int[size];
+        for (int i = 0; i < unionId.length; i++) {
+            unionId[i] = i;
+        }
     }
 
     @Override
@@ -12,24 +15,28 @@ public class UnionFindV1 implements UnionFind {
         return unionId.length;
     }
 
-    private int find(int p){
+    private int find(int p) {
         return unionId[p];
     }
 
     /**
      * O(n)
+     *
      * @param p
      * @param q
      */
     @Override
     public void union(int p, int q) {
-        if(p < 0 || p >= unionId.length || q <0 || q >= unionId.length){
+        if (p < 0 || p >= unionId.length || q < 0 || q >= unionId.length) {
             throw new IllegalArgumentException("invail index");
         }
         int pId = find(p);
         int qId = find(q);
-        for(int i = 0 ; i < unionId.length; i++){
-            if(unionId[i] == qId){
+        if (pId == qId) {
+            return;
+        }
+        for (int i = 0; i < unionId.length; i++) {
+            if (unionId[i] == qId) {
                 unionId[i] = pId;
             }
         }
@@ -37,13 +44,14 @@ public class UnionFindV1 implements UnionFind {
 
     /**
      * O(1)
+     *
      * @param p
      * @param q
      * @return
      */
     @Override
     public boolean isConnected(int p, int q) {
-        if(p < 0 || p >= unionId.length || q <0 || q >= unionId.length){
+        if (p < 0 || p >= unionId.length || q < 0 || q >= unionId.length) {
             throw new IllegalArgumentException("invail index");
         }
         return find(p) == find(q);
