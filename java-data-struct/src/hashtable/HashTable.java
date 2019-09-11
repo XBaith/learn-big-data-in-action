1e61385669af755a90d1b6c5916425e237809b1f
XBaith <tocreationbai@gmail.com>
2019-09-11 21:55:27 +0800

    Update HashTable

--- java-data-struct/src/hashtable/HashTable.java
+++ java-data-struct/src/hashtable/HashTable.java
@@ -1,4 +1,91 @@
 package hashtable;
 
-public class HashTable {
+import java.util.TreeMap;
+
+public class HashTable<K, V> {
+    private final int[] capacity
+            = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
+            49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469,
+            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
+    private final static int upperTol = 10;
+    private final static int lowerTol = 2;
+    private static int initIndex = 0;
+    private TreeMap<K, V>[] hashTable;
+    private int M;
+    private int size;
+
+    public HashTable() {
+        this.M = capacity[initIndex];
+        hashTable = new TreeMap[M];
+        for(int i = 0 ; i < M; i++){
+            hashTable[i] = new TreeMap<>();
+        }
+        size = 0;
+    }
+
+    private int hash(K key){
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public V get(K key){
+        return hashTable[hash(key)].get(key);
+    }
+
+    public void add(K key, V value){
+        TreeMap map = hashTable[hash(key)];
+        if(map.containsKey(key)){
+            map.put(key, value);
+        }else{
+            map.put(key, value);
+            size++;
+            if(size >= upperTol * M){
+                resize(capacity[initIndex++]);
+            }
+        }
+    }
+
+    private void resize(int newM) {
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
+        for(int i = 0 ; i < newM; i++){
+            newHashTable[i] = new TreeMap<>();
+        }
+        int oldM = M;
+        this.M = newM;
+        for(int i = 0 ; i < oldM; i++){
+            TreeMap<K, V> map = hashTable[i];
+            for(K key : map.keySet()) {
+                newHashTable[hash(key)].put(key, map.get(key));
+            }
+        }
+        this.hashTable = newHashTable;
+    }
+
+    public V remove(K key){
+        TreeMap<K, V> map = hashTable[hash(key)];
+        if(!map.containsKey(key)){
+            throw new IllegalArgumentException(key + " is not exist");
+        }
+        V result = map.remove(key);
+        size--;
+        if(size < lowerTol * M && M >= capacity[0]){
+            resize(capacity[initIndex--]);
+        }
+        return result;
+    }
+
+    public void set(K key, V value){
+        TreeMap map = hashTable[hash(key)];
+        if(!map.containsKey(key)){
+            throw new IllegalArgumentException(key + " is not exist");
+        }
+        map.put(key, value);
+    }
+
+    public boolean contains(K key){
+        return hashTable[hash(key)].containsKey(key);
+    }
 }
--- pragmatic-scala/programming-in-scala/src/rational/Rational.scala
+++ pragmatic-scala/programming-in-scala/src/rational/Rational.scala
@@ -1,5 +1,24 @@
-package `object`
+package rational
 
-class Rational {
+class Rational(n: Int, d: Int) {
+  require(d != 0)/*前置条件除数不能为0*/
+  override def toString: String = n + " / " + d /*重写toString方法*/
 
+  val number = n  /*用常量表示出来，后面就可以访问构造函数中的n与d，如果不写出字段就访问不到类中的d与n*/
+  val damon = d
+  def +(that: Rational) = new Rational(number * that.damon + that.number * damon, damon * that.damon)
+  def -(that: Rational) = new Rational(number * that.damon - that.number * damon, damon * that.damon)
+
+  def *(that: Rational) = new Rational(number * that.number, damon * that.damon)
+  def /(that: Rational) = new Rational(number * that.damon, damon * that.number)
+}
+
+object Main extends App {
+  val left = new Rational(2, 3)
+  val right = new Rational(1, 5)
+  //同分后：10/15, 3/15
+  println(left + right)
+  println(left - right)
+  println(left * right)
+  println(left / right)
 }
