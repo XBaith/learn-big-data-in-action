e7d9c051accafc421b264baa85c1506ecfd111a4
¨XBaith¨ <tocreationbai@gmail.com>
2019-09-14 16:31:31 +0800

    Learn Interrupte Thread

---
+++ java-concurrency/src/InterruptThread.java
@@ -0,0 +1,41 @@
+/**
+ * 在抛出InterredException之后会重置interrupt属性
+ */
+public class InterruptThread {
+
+    public static void main(String[] args) {
+        //睡眠线程
+        Thread sleep = new Thread(new SleepRunner(), "SleepThread");
+        sleep.setDaemon(true);
+        Thread busy = new Thread(new BusyRunner(), "BusyThread");
+        busy.setDaemon(true);
+        sleep.start();
+        busy.start();
+        //为了线程充分启动
+        SleepUtils.seconds(5);
+
+        sleep.interrupt();
+        busy.interrupt();
+
+        System.out.println(sleep.getName() + ": " + sleep.isInterrupted());
+        System.out.println(busy.getName() + ": " + busy.isInterrupted());
+        //防止线程立即退出
+        SleepUtils.seconds(2);
+    }
+
+    static class SleepRunner implements Runnable{
+        @Override
+        public void run() {
+            while(true){
+                SleepUtils.seconds(10);
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable{
+        @Override
+        public void run() {
+            while(true){}
+        }
+    }
+}
